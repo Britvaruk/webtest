de792821fcdf0c6453c59d5b919264279ab6e2cf
hannyS1 <59133695+hannyS1@users.noreply.github.com>
2020-05-06 17:49:12 +0700

    dfgd
    v 1.0.1

--- scripts/front.js
+++ scripts/front.js
@@ -1,6 +1,23 @@
 function hello() {
-   document.getElementById('lk').innerHTML = "иди нахуй"
+   document.getElementById('lk').innerHTML = "иди нахуй";
 }
 function getReqistration(){
    document.getElementById('registration').hidden = false;
+}
+function takeAccount() {
+   var data = {
+      login: document.getElementById("login").value,
+      password: document.getElementById("password").value,
+      confirmPassword: document.getElementById("confirmPassword").value,
+      email: document.getElementById("email").value
+   }
+   if(data.password == data.confirmPassword){
+      console.log(data);
+   }
+   else {
+      console.log("Пароли не совпадают");
+   }
+
+
+
 }
